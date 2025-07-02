42be7f291825143ca550abfe3cbb5935a4cbdbad
Krish Suthar 17 <krishsuthar711@gmail.com>
2025-07-02 21:12:26 +0530

    work almost done padding work is addind aos in to it that's it

--- script.js
+++ script.js
@@ -113,3 +113,9 @@ model.addEventListener("load", () => {
     console.log("Animation paused at 2s ✅");
   }, 1800); // 2 seconds
 });
+
+// aos
+AOS.init({
+  duration: 1000, // animation duration in ms
+  once: true, // whether animation should happen only once - while scrolling down
+});
